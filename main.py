db45b943de4a3550e64eaf56089130a07432267a
dmitry <d.filin64@gmaul.com>
2017-02-24 23:36:12 +0300

    Spacial Map has been done

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ from data.constants import *
 while (True):
     env = obj.Env()
     cell = (0, 0)
-    thsom = obj.THSOM()
-    while (True):
+    thsom = obj.THSOM(20, 16)
+    for T in range(1000):
+        print ("TIME = ", T)
         action = ACTIONS[np.random.randint(4)]
-        print (action)
+        print ("ACTION = ", action)
         cell, reward, is_done, is_block_changed = env.step(cell, action)
-        input("Press enter")
-        env.get_block(cell)
+        if is_block_changed:
+            block = env.get_block(cell)[1]
+            in_vec = block.ravel().T
+            print ("Input Vector ", in_vec)
+            bmu = thsom.get_bmu(in_vec)
+            print ("BMU ", bmu)
+            thsom.update_sm_weights(bmu, T, in_vec)
+            # input("Press enter.")
         if is_done:
             break
+    print (thsom.sm)
+    input("Press enter.")
--- objects.py
+++ objects.py
@@ -18,10 +18,11 @@ class Env:
     def get_block(self, cell):
         i, j = cell
         left_bound = j // BLOCK_SIZE[1]
-        right_bound = j + BLOCK_SIZE[1]
+        right_bound = left_bound + BLOCK_SIZE[1]
         up_bound = i // BLOCK_SIZE[0]
-        down_bound = i + BLOCK_SIZE[0]
-        block = self.maze[up_bound:down_bound][left_bound:right_bound]
+        down_bound = up_bound + BLOCK_SIZE[0]
+        np_maze = np.array(self.maze)
+        block = np_maze[up_bound:down_bound, left_bound:right_bound]
         np_block = [[0 if j in ['0', 'A', 'S', 'F'] else 1 for j in i] for i in block]
         return left_bound + up_bound*BLOCK_SIZE[0], np.array(np_block)
     def step(self, cell, action):
@@ -30,7 +31,7 @@ class Env:
         if i < 0 or j < 0 or i >= MAZE_SIZE[0] or j >= MAZE_SIZE[1]:
             return cell, None, None, None
         if self.maze[i][j] == WALL:
-            return cell, WALL_PUN, True, None
+            return cell, WALL_PUN, False, None
         if self.maze[i][j] == HOLE:
             return (i, j), HOLE_PUN, True, None
         if self.maze[i][j] == FIN:
@@ -40,7 +41,7 @@ class Env:
         self.show()
         is_block_changed = bool(self.get_block(cell)[0]-self.get_block((i, j))[0])
         if is_block_changed:
-            print ("BLOCK CHANGED" + self.get_block(i, j)[0])
+            print ("BLOCK CHANGED ", self.get_block((i, j))[0])
         return (i, j), 0, False, is_block_changed
 class THSOM:
     sm = np.zeros((0, 0))
@@ -48,8 +49,8 @@ class THSOM:
     neurons_num = 0
     def __init__(self, neurons_num, dim):
         self.neurons_num = neurons_num
-        sm = np.random.randint(1, size=(dim, neurons_num))
-        tm = np.random.rand((neurons_num, neurons_num))
+        self.sm = np.random.rand(dim, neurons_num)
+        self.tm = np.random.rand(neurons_num, neurons_num)
     def get_bmu(self, vec):
         mn = 1e9
         bmu = 0
@@ -57,4 +58,15 @@ class THSOM:
             dist = np.linalg.norm(self.sm[:,i] - vec)
             if (dist < mn):
                bmu = i
-        return bmu
+        return self.sm[:,bmu]
+    def update_sm_weights(self, bmu, t, vec):
+        rad = R0 * np.exp(-np.sqrt(t) / R1)
+        for i in range(self.neurons_num):
+            dist = np.linalg.norm(bmu - self.sm[:,i])
+            if dist < rad:
+                print ("SPATIAL VECTOR ", i)
+                SLR = S0 * np.exp(-dist*dist / S1)
+                TLR = T0 * np.exp(-t / T1)
+                DIST = vec - self.sm[:,i]
+                self.sm[:,i] += SLR * TLR * DIST
+                print ("SLR = ", SLR, "TLR = ", TLR, "DIST = ", DIST)
