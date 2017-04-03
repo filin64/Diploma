0a36eea17423973df3d8a163df004c177b5b6159
dmitry <d.filin64@gmaul.com>
2017-04-03 19:16:32 +0300

    Q-net was added

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import objects as obj
 import numpy as np
 from data.constants import *
+import time
 
-ff = lambda x: "%.2f" % x
+ff = lambda x: "%.5f" % x
 np.set_printoptions(formatter={'float_kind':ff})
+thsom = obj.THSOM(NN, 16)
+q_net = obj.QNet()
+
 while (True):
     env = obj.Env()
-    cell = (0, 0)
+    position = (0, 0)
+    env.show(position)
     for T in range(1000):
-        print ("TIME = ", T)
-        action_ind = np.random.randint(4)
-        action = ACTIONS[action_ind]
-        print ("ACTION = ", ACTIONS_WORDS[action_ind])
-        cell, reward, is_done = env.step(cell, action)
+        if LOG_ON: print ("TIME = ", T)
+        block = env.return_block_as_vector(position) #where we are?
+        ibmu = thsom.get_bmu(block) #Index of BMU
+        if LOG_ON: print ("BMU", ibmu)
+        thsom.update_sm_weights(ibmu, T, block)
+        if np.random.rand() <= 0.35:  #Greedy Policy
+            best_action = np.random.randint(len(ACTIONS))
+        else:
+            best_action = q_net.predict(thsom.sm[:,ibmu])
+        if LOG_ON: print ('Best Action', ACTIONS_WORDS[best_action])
+        action = ACTIONS[best_action]
+        position, reward, is_done = env.step(position, action)
+        if LOG_ON: print ('Reward', reward)
+        # for i in range(thsom.neurons_num):
+        #     print('Neuron', i)
+        #     thsom.get_neuron_as_block(i)
+        q_net.back_prop(reward, thsom.sm[:,ibmu], best_action)
+        if LOG_ON: q_net.print_weights()
+        # input("Press Enter")
+        time.sleep(1)
+        # os.system('clear')
         if is_done:
             break
-        input("Press enter.")
--- objects.py
+++ objects.py
@@ -1,14 +1,24 @@
 import numpy as np
 from data.constants import *
 from termcolor import colored
+from collections import deque
 
 class Env:
     maze = []
+    num_maze = []
     def __init__(self):
         f = open(FILE_PATH)
         self.maze = [list(l.strip('\n')) for l in f.readlines()]
+        self.num_maze = np.zeros((MAZE_SIZE))
+        for i in range (MAZE_SIZE[0]):
+            for j in range (MAZE_SIZE[1]):
+                if self.maze[i][j] == WALL:
+                    self.num_maze[i, j] = 1
+                else:
+                    self.num_maze[i, j] = 0
+        self.update((0, 0), 'A')
     #---------------------------------------------------------------------------------------------------#
-    # Function to display process of agent's movement
+    # To display process of agent's movement
     def show(self, position):
         left, right, up, down = self.get_block(position)
         for i in range(np.shape(self.maze)[0]):
@@ -23,26 +33,31 @@ class Env:
     def update(self, cell, val):
         self.maze[cell[0]][cell[1]] = val
     #---------------------------------------------------------------------------------------------------#
-    # Making a step function. It returns all neccessary data which is needed to define strategy
+    # It returns all neccessary data which is needed to define strategy
     # Return values: new_cell, reward, is_done
     def step(self, position, action):
         i, j = np.array(position) + np.array(action)
+        # if we are out of bounds
         if i < 0 or j < 0 or i >= MAZE_SIZE[0] or j >= MAZE_SIZE[1]:
             self.update(position, 'A')
             self.show(position)
-            return position, None, None
+            return position, BOUND_PUN, None
         if self.maze[i][j] == WALL:
-            return position, WALL_PUN, False
+            self.update(position, 'A')
+            self.show(position)
+            return position, WALL_PUN, True
         if self.maze[i][j] == HOLE:
             return (i, j), HOLE_PUN, True
         if self.maze[i][j] == FIN:
+            self.update((i, j), '*')
+            self.show((i, j))
             return (i, j), FIN_PUN, True
         self.update(position, '0')
         self.update((i, j), 'A')
         self.show((i, j))
-        return (i, j), 0, False
+        return (i, j), REWARD, False
     # ---------------------------------------------------------------------------------------------------#
-    # Defining 4x4 Block were we are now
+    # 4x4 Block were we are now
     def get_block(self, position):
         i, j = position
         left = j - BLOCK_SIZE[1]/2
@@ -66,6 +81,11 @@ class Env:
             down = MAZE_SIZE[0] - 1
             up = MAZE_SIZE[0] - 1 - BLOCK_SIZE[0]
         return (left, right, up, down)
+    # ---------------------------------------------------------------------------------------------------#
+    # Numerical value of block
+    def return_block_as_vector(self, position):
+        left, right, up, down = self.get_block(position)
+        return self.num_maze[int(up):int(down)+1, int(left):int(right)+1].ravel()
 
 
 class THSOM:
@@ -73,38 +93,89 @@ class THSOM:
     tm = np.zeros((0, 0))
     neurons_num = 0
     dm = np.zeros((0, 0))
-    sm2d = np.zeros((0, 0))
     def __init__(self, neurons_num, dim):
+        #dim - length of vectors
         self.neurons_num = neurons_num
         self.sm = np.random.rand(dim, neurons_num)
         self.tm = np.random.rand(neurons_num, neurons_num)
-        self.dm = np.zeros((neurons_num, neurons_num))
     def get_bmu(self, vec):
         mn = 1e9
         bmu = 0
         for i in range(self.neurons_num):
-            dist = np.linalg.norm(self.sm[:,i] - vec)
+            dist = self.dist(x=vec, y=self.sm[:,i])
             if (dist < mn):
                bmu = i
                mn = dist
         return bmu
     def update_sm_weights(self, ibmu, t, vec):
+        #ibmu - index of bmu, t - moment of time, vec - input vector
         bmu = self.sm[:, ibmu]
-        rad = R0 * np.exp(-t / R1)
-        print ("RADIUS = ", rad)
+        rad = max (10**(-10), R0 * np.exp(-t / R1))
+        # rad = 10**(-10)
+        if LOG_ON: print ("RADIUS = ", rad)
         for i in range(self.neurons_num):
-            dist = np.linalg.norm(bmu - self.sm[:,i])
+            dist = self.dist(x=self.sm[:,i], y=bmu)
             if dist < rad:
-                print ("SPATIAL VECTOR ", i)
-                SLR = S0 * np.exp(-dist*dist / S1)
+                if LOG_ON: print ("SPATIAL VECTOR ", i)
+                SLR = S0 * np.exp(-dist*dist/S1)
                 TLR = T0 * np.exp(-t / T1)
                 DIFF = vec - self.sm[:,i]
-                print ("Before ", self.sm[:,i])
+                if LOG_ON: print ("Before ", self.sm[:,i])
                 self.sm[:,i] += SLR * TLR * DIFF
-                print ("SLR = ", SLR, "TLR = ", TLR, "SLR * TLR = ", SLR * TLR)
-                print ("After ", self.sm[:,i])
-    def dist_matrix(self):
-        for i in range(self.neurons_num):
-            for j in range(self.neurons_num):
-                self.dm[i, j] = np.linalg.norm(self.sm[:,i] - self.sm[:,j])
-        print(self.dm)
+                if LOG_ON: print ("Dist", dist, "SLR = ", SLR, "TLR = ", TLR, "DIFF", DIFF)
+                if LOG_ON: print ("After ", self.sm[:,i])
+    def dist(self, x, y):
+        # x - input, y - neuron
+        y = [np.uint64(1) if i > 0.5 else np.uint64(0) for i in y]
+        x = [np.uint64(1) if i > 0.5 else np.uint64(0) for i in x]
+        alpha = 0.5
+        betta = 5
+        y_ld = deque(y) #for left shift
+        y_rd = deque(y) #for right shift
+        d = dict()
+        shift = 0
+        d[shift] = sum(np.bitwise_xor(x, y))
+        # shift left
+        while y_ld[0] == 0 and abs(shift) < len(y_ld):
+            y_ld.rotate(-1)
+            y_list_ld = list(np.uint64(i) for i in y_ld)
+            shift -= 1
+            s = sum(np.bitwise_xor(x, y_list_ld))
+            d[abs(shift)] = s
+        shift = 0
+        while y_rd[-1] == 0 and abs(shift) < len(y_rd):
+            y_rd.rotate(1)
+            y_list_rd = list(np.uint64(i) for i in y_rd)
+            shift += 1
+            s = sum(np.bitwise_xor(x, y_list_rd))
+            d[abs(shift)] = s
+        #ind - количество сдвигов
+        ind = min(d, key=lambda i: d[i])
+        ans = alpha * (1 - np.exp(-ind / betta)) + (1 - alpha) * (1 - np.exp(-d[ind] / betta))
+        return ans
+    def get_neuron_as_block(self, i):
+        x = self.sm[:,i]
+        for i in range(len(x)):
+            if x[i] > 0.5:
+                print('#', end='')
+            else:
+                print ('0', end='')
+            if (i + 1) % 4 == 0:
+                print ()
+class QNet:
+    weights = np.zeros((0, 0))
+    n = 16
+    def __init__(self):
+        self.weights = np.random.rand(self.n, 4)
+    def predict(self, x):
+        # x - input signal 16x1
+        ans = np.dot(x.T, self.weights)
+        if LOG_ON: print ('Distribution', ans)
+        return np.argmax(ans)
+    def back_prop(self, reward, x, ind):
+        #x - input vector, y - output answer
+        old_weights = np.copy(self.weights[:, ind])
+        self.weights[:, ind] = self.weights[:, ind] + x * reward + np.ones(self.n).T * DELTA
+        if LOG_ON: print ('Weights Delta', self.weights[:, ind] - old_weights)
+    def print_weights(self):
+        print (self.weights)
